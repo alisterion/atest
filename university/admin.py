a9e069bcb5350ed10e87ba5d0bea087528fdfab2
Alex <a.vorobyov@bvblogic.com>
2019-06-03 14:39:55 +0300

    * university structure and admin

--- university/admin.py
+++ university/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import University
 
-# Register your models here.
+
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner')
--- university/models.py
+++ university/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=512, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Universities"
+
+    def __str__(self):
+        return "%s_%s" % (self.name, self.owner)
+
+    def __repr__(self):
+        return "%s_%s" % (self.name, self.owner)
